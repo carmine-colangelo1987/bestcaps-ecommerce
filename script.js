a8357c147a599017eedb035dafba51f6280745ce
Fiorenza PIEROTTI <fiorenza.pierotti@alten.it>
2020-06-24 12:05:48 +0200

    design

--- script.js
+++ script.js
@@ -61,7 +61,7 @@ function productCard(item){
                     <div class="card-footer card-footer-product">
                         <small class="text-muted pr-3">Aggiungi al carrello</small>
                         <div class="trolley">
-                            <div class="trolley-number">2</div>
+                            <div class="trolley-number"></div>
                             <div class="trolley-add"><i class="fas fa-plus-square" onclick="add()"></i><i class="fas fa-minus-square"></i></div>
                             <div class="trolley-icon"><i class="fas fa-shopping-cart" aria-hidden="true"></i></div>
                         </div>
